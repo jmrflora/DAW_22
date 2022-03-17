bd0a56a09ce33195171eb4260047294c3fe2cb09
José Matheus R. Flora <60922589+jmrflora@users.noreply.github.com>
2022-03-17 10:50:16 -0300

    Add files via upload

---
+++ exercicio_numeros.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1></h1>
+        <?php
+        $var = 12;
+        var_dump(is_int($var));
+        echo gettype($var);
+        echo "<br>";
+        echo pi();
+        echo "<br>";
+        echo "o maior valor entre 10 70 88 42 e: ".max(10,70,88,42);
+        ?>
+    </body>
+</html>
---
+++ exercicio_operacao.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>exercicio operacoes</h1>
+        <?php
+        $var = 1+1;
+        echo "adicao 1+1: ".$var;
+        echo "<br>";
+        $var = 1-1;
+        echo "subtracao 1-1: ".$var;
+        echo "<br>";
+        $var = 10 * 10;
+        echo "multiplicacao 10*10: ".$var;
+        echo "<br>";
+        $var = 10/10;
+        echo "divisao 10/10: ".$var;
+        echo "<br>";
+        $var = 10%3;
+        echo "modulo 10%3: ".$var;
+        echo "<br>";
+        $var = 2**2;
+        echo "exponencia 2**2: ".$var;
+        echo "<br>";
+        ?>
+    </body>
+</html>
