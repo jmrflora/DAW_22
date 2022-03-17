76cc1fd4ac760b28233aaabf7b81bc59cdbe8970
José Matheus R. Flora <60922589+jmrflora@users.noreply.github.com>
2022-03-17 08:22:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+    <h1>3DAW</h1>
+    <?php
+    $var = "based string";
+    echo "<h2>exercicio 1</h2>";
+    echo "alo bom dia minha gente<br>";
+    echo $var;
+    $var = 1 + 1;
+    echo "<br>based number: ".$var;
+    ?>
+    <p>exemplo de php dentro do <?php echo "paragrafo"?>, legal né?</p>
+</body>
+</html>
